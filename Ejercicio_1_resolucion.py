1b3c3bdc000be241047251a667df5eb7f65c5b1e
ossertive <ossertive1993@gmail.com>
2026-08-17 13:31:22 -0300

    Add files via upload

---
+++ Ejercicio_1_resolucion.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 12:49:29 2026
+
+@author: Anoma
+"""
+#%% resolucion Ejercicio 1 de la guia
+import sys
+sys.float_info.epsilon # el numero sueperior mas chiquito que puede representar la maquina.
+-sys.float_info.epsilon #El limite inferior mas chico que puede representar la maquina
+sys.float_info.min # 
+sys.float_info.max
+sys.float_info.radix
+sys.float_info.mant_dig
+sys.maxsize
