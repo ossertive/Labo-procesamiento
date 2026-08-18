1271785576fbc080b74b6bb7a50ce002fbcc8585
ossertive <ossertive1993@gmail.com>
2026-08-18 13:48:11 -0300

    Add files via upload

---
+++ inversa.py.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 14:43:29 2026
+
+@author: Anoma
+"""
+
+# programa para calcular la inversa de un número.
+a=imput("ingrese un número:")
+X=float(a)
+inversa_x=1/X
+print(inversa_x)
