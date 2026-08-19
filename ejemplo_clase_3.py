eb0b31a83ec0bc7102d87cf4ab27aa017055bef7
ossertive <ossertive1993@gmail.com>
2026-08-19 14:19:00 -0300

    Add files via upload

---
+++ ejemplo_clase_3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 16:54:49 2026
+
+@author: Anoma
+"""
+import os
+os.getcwd()
+#%% Resolución ejercicio 3.
+nombre=input("ingrese su nombre:")
+materia=input("ingrese la materia que esta cursando:")
+aula=input("número de aula:")
+pabe=input("ingrese el número de pabellon en el que este cursando:")
+turno=input("Turno de cursada:")
+print(f"Bienvenido {nombre} a Exactas!. Te esperamos en {materia} en el aula {aula}, pab.{pabe} por la {turno}.")
