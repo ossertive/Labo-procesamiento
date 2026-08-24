36d1bb66dd755b5b77a61f7e4ee06c8fd2b88952
ossertive <ossertive1993@gmail.com>
2026-08-24 16:21:13 -0300

    Add files via upload

---
+++ practica_02/Ejercicio_1_resolucion.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 12:13:48 2026
+
+@author: Gastón Orozco
+Practica 2
+"""
+# %% Ejercicio 1 
+b=5 #es una variable real
+c=10 # es una variable real
+a=3<4 # es una variable booleana (true or false)
+a=b<=c # true
+a=b==c # false
+a=b!=c # true
+# %%
+a= True
+b=False
+isinstance(a,bool) # true
+a and b # False (True si ambas son True)
+a or b # true si a o b es true 
+not b # True (not niega el valor booleano)
+isinstance(a or b, bool) # true 
---
+++ practica_02/Ejercicio_2_resolucion.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 15:12:26 2026
+
+@author: Anoma
+"""
+# %% Resolución ejercicio 2
+a=False
+b=False
+a and b # False (True si ambas son True)
+a or b # False (true si alguna es True)
+a==b # True 
+a!=b # False 
+a and(not b) # False(not niega el valor booleano)
+# %%
+a=True
+b=False
+a and b # False
+a or b # True 
+a==b # False 
+a!=b # True
+a and (not b) # true 
