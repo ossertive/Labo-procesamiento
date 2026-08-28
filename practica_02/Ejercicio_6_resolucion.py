8978991fec1d2aaf8cd76f02fd978f4dc40c4b8f
ossertive <ossertive1993@gmail.com>
2026-08-28 10:27:54 -0300

    Add files via upload

---
+++ practica_02/Ejercicio_6_resolucion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 28 10:03:42 2026
+
+@author: Estudiante
+"""
+# %%
+sueldo=float(input("ingrese su sueldo:"))
+if 0>=sueldo<6000:
+    print("no paga impuesto")
+elif 6000<=sueldo<20000:
+    impuesto=(sueldo-6000)*0.17
+    print(f"El impuesto a pagar: {impuesto}")
+elif ...:
+    
